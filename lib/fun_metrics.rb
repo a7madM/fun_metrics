f63624417a810c5f42c7f35577498b314fd40d33
Ahmed Magdy <ahmadmgdi94@gmail.com>
2025-08-24 20:58:45 +0200

    first commit

---
+++ lib/fun_metrics.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fun_metrics/version"
+
+module FunMetrics
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/fun_metrics_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe FunMetrics do
+  it "has a version number" do
+    expect(FunMetrics::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
